431a017cdf35588c4872cf176d7f5650f1f22614
Hari Prasad <haririo321@gmail.com>
2025-10-17 18:59:43 +0530

    modified leetcode 94

--- leetcode-94/src/main.rs
+++ leetcode-94/src/main.rs
@@ -1,10 +1,10 @@
-use std::{cell::RefCell, env::current_exe, rc::Rc};
+use text_io::read;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
     pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
+    pub left: Option<Box<TreeNode>>,
+    pub right: Option<Box<TreeNode>>,
 }
 
 impl TreeNode {
@@ -18,24 +18,45 @@ impl TreeNode {
     }
 }
 
-fn main() {
-    let mut root = TreeNode::new(1);
-    let current = &mut root;
-    current.right = Some(Rc::new(RefCell::new(TreeNode::new(3))));
-    let mut current = current.right.as_ref().unwrap().borrow_mut();
-    current.left = Some(Rc::new(RefCell::new(TreeNode::new(2))));
-    println!(
-        "{:?}",
-        inorder_traversal(Some(Rc::new(RefCell::new(current))))
-    );
+fn add_node(head: &mut TreeNode) {
+    let ch: i32;
+    let input: i32;
+    let mut current = head;
+    if current.left.is_none() && current.right.is_none() {
+        println!("Which node? (0/1): ");
+        ch = read!();
+        if ch == 0 {
+            println!("Enter a value: ");
+            input = read!();
+            current.left = Some(Box::new(TreeNode::new(input)));
+            current = current.left.as_mut().unwrap();
+        } else {
+            println!("Enter a value: ");
+            input = read!();
+            current.right = Some(Box::new(TreeNode::new(input)));
+            current = current.right.as_mut().unwrap();
+        }
+    } else if current.left.is_none() {
+        println!("Enter a value for the left node: ");
+        input = read!();
+        current.left = Some(Box::new(TreeNode::new(input)));
+        current = current.left.as_mut().unwrap();
+    } else if current.right.is_none() {
+        println!("Enter a value for the right node: ");
+        input = read!();
+        current.right = Some(Box::new(TreeNode::new(input)));
+        current = current.right.as_mut().unwrap();
+    }
 }
 
-fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
-    let mut res_vector: Vec<i32> = vec![];
-    let mut current = root.clone();
-    while current.is_some() {
-        res_vector.push(current.as_ref().unwrap().as_ref().borrow().val.clone());
-        current = None;
-    }
-    res_vector
+fn inorder_traversal(head: &mut TreeNode) {
+    inorder_traversal(head.left.as_mut().unwrap());
+    println!("{}", head.val);
+    inorder_traversal(head.right.as_mut().unwrap());
+}
+
+fn main() {
+    let mut head = TreeNode::new(0);
+    add_node(&mut head);
+    inorder_traversal(&mut head);
 }
