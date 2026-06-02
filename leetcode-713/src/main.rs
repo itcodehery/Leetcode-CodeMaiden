b5ef2b10a6a75db7a529800f265bff39ce5d8071
Hari Prasad <haririo321@gmail.com>
2026-06-02 21:02:30 +0530

    modified leetcode 713

---
+++ leetcode-713/src/main.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn num_subarray_product_less_than_k(nums: Vec<i32>, k: i32) -> i32 {
+        let super_array: Vec<Vec<i32>>;
+        let element_counter: i32 = 0;
+        for i in 0..2 ^ nums.len() {
+            super_array.push();
+        }
+
+        0
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
